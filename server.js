4790d8e794efbfd64c4fe06d1b388449a5c8fe15
Ba7arathinam <bala01225@gmail.com>
2023-02-22 12:49:36 +0530

    who

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const dbConfig=require('./db');
 const roomsRoute=require('./route/roomsRoute');
 const usersRoute=require('./route/usersRoute');
 const bookingRoute=require('./route/bookingRoute');
-const port=process.env.BASE_URL
+const BOO='https://zinrooms-bookings.onrender.com'
+const port=process.env.BOO || process.env.PORT
 app.use(express.json())
 
 app.use('/api/rooms',roomsRoute)
