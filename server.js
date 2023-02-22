183ba02c32661127cc0c12c66e06a4fc03954709
Ba7arathinam <bala01225@gmail.com>
2023-02-22 16:21:02 +0530

    woow

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const dbConfig=require('./db');
 const roomsRoute=require('./route/roomsRoute');
 const usersRoute=require('./route/usersRoute');
 const bookingRoute=require('./route/bookingRoute');
-const port= process.env.PORT ||6010
+const BASE_PORT= process.env.BASE_PORT
 app.use(express.json())
 
 app.use('/api/rooms',roomsRoute)
 app.use('/api/users',usersRoute)
 app.use('/api/bookings',bookingRoute)
-app.listen(port,()=> console.log(`server run successfully in ${port} 🔥`));
+app.listen(BASE_PORT,()=> console.log(`server run successfully in ${BASE_PORT} 🔥`));
